5e7a5cf95ea87ebd69605fde222f1b76ca3d4e64
Apoki21 <a.nabilmudzakki@gmail.com>
2020-10-14 16:08:23 +0700

    Upload Vue

---
+++ src/components/ModelBrinxid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="instagram-photo">
+    <div class="insta-item set-bg" style="background-image: url('/img/m_dark01.png')">
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5><a href="#">Brinx_gallery</a></h5>
+      </div>
+    </div>
+    <div class="insta-item set-bg" style="background-image: url('/img/w_dark02.png')">
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5><a href="#">Brinx_gallery</a></h5>
+      </div>
+    </div>
+    <div class="insta-item set-bg" style="background-image: url('/img/d_pbo.jpg')">
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5><a href="#">Brinx_gallery</a></h5>
+      </div>
+    </div>
+    <div class="insta-item set-bg" style="background-image: url('/img/w_elite.png')">
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5><a href="#">Brinx_gallery</a></h5>
+      </div>
+    </div>
+    <div class="insta-item set-bg"  style="background-image: url('/img/m_pbo01.png')">
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5><a href="#">Brinx_gallery</a></h5>
+      </div>
+    </div>
+    <div class="insta-item set-bg" style="background-image: url('/img/w_pbo02.png')">
+      <div class="inside-text">
+        <i class="ti-instagram"></i>
+        <h5><a href="#">Brinx_gallery</a></h5>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    name: "ModelBrinxid"
+}
+</script>
